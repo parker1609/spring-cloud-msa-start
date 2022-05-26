ec3cf94e9074c6e39f99bdc84370ccbf2c08a6d5
parker1609 <psbum1609@gmail.com>
2022-05-27 08:54:41 +0900

    create user-service sub module

---
+++ user-service/src/test/java/me/parker/springcloudmsastart/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.parker.springcloudmsastart.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
